7e54580a6ac245594e71b851a6691486de5c587f
Afonso Dutra Nogueira Filho <afonso.nogueira@outlook.com.br>
2019-03-01 09:51:18 -0300

    first update

--- AfonsoftIPTV/AfonsoftIPTV/App.xaml.cs
+++ AfonsoftIPTV/AfonsoftIPTV/App.xaml.cs
@@ -15,7 +15,7 @@ namespace AfonsoftIPTV
          * This imposes a limitation in which the App class must have a default constructor. 
          * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
          */
-        public App() : this(null) { }
+        public App() : this(null) { } 
 
         public App(IPlatformInitializer initializer) : base(initializer) { }
 
